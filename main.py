1b41b536a5557b3098a0f30be5eb359a1d0bb73c
Joy Liu <liujoy57@gmail.com>
2024-04-08 16:42:10 -0700

    fixed noise

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 
-texture = torchvision.io.read_image('textures/brickwall2.png')
+texture = torchvision.io.read_image('textures/tomatoes.png')
 plt.imshow( texture.permute(1, 2, 0)  )
 plt.show()
 
-num_samples = texture.shape[0] * texture.shape[1] * texture.shape[2]
+num_samples = texture.shape[1] * texture.shape[2] 
 p = torch.ones(num_samples)/num_samples
 index = torch.multinomial(input=p, num_samples=num_samples, replacement=True)
-init = texture.flatten()[index].reshape(texture.shape)
-plt.imshow( init.permute(1, 2, 0)  )
+init = texture.permute((1,2,0)).view((num_samples, -1))[index].reshape(texture.shape[1], texture.shape[2], texture.shape[0])
+plt.imshow( init )
 plt.show()
