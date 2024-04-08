abc36b9eb7b959a2e4d5c0a1565e934c069ed99d
Joy Liu <liujoy57@gmail.com>
2024-04-08 16:45:46 -0700

    cleaner

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import torch
 import torchvision
 import matplotlib.pyplot as plt
 
-texture = torchvision.io.read_image('textures/tomatoes.png')
-plt.imshow( texture.permute(1, 2, 0)  )
+texture = torchvision.io.read_image('textures/tomatoes.png').permute(1, 2, 0)
+plt.imshow( texture  )
 plt.show()
 
-num_samples = texture.shape[1] * texture.shape[2] 
+num_samples = texture.shape[0] * texture.shape[1] 
 p = torch.ones(num_samples)/num_samples
 index = torch.multinomial(input=p, num_samples=num_samples, replacement=True)
-init = texture.permute((1,2,0)).view((num_samples, -1))[index].reshape(texture.shape[1], texture.shape[2], texture.shape[0])
+init = texture.view((num_samples, -1))[index].reshape(texture.shape)
 plt.imshow( init )
 plt.show()
