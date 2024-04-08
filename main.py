9ba4bd203551bb5701f6a7e46a568fe9079b145e
Joy Liu <liujoy57@gmail.com>
2024-04-08 16:01:42 -0700

    show img

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import pytorch
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+
+texture = torchvision.io.read_image('textures/tomatoes.png')
+plt.imshow( texture.permute(1, 2, 0)  )
+plt.show()
