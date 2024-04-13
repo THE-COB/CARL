c26bed058c269cb579a18a3b7acbc93c10826273
Joy Liu <liujoy57@gmail.com>
2024-04-12 19:18:12 -0700

    search fix

--- search.py
+++ search.py
@@ -5,6 +5,8 @@ from docarray import DocumentArray
 #import pynndescent
 import einops 
 import matplotlib.pyplot as plt
+import shutil 
+import os
 
 class Search:
 	def __init__(self, exemplar, rank=5): 
@@ -12,7 +14,6 @@ class Search:
 		self.rank = rank
 		self.set_pca_coordinates(exemplar)
 		
-	
 	def set_pca_coordinates(self, exemplar):
 		# following paper, 8x8 patches 
 		samples = []
@@ -26,28 +27,30 @@ class Search:
 		self.pca_to_samples = {}
 		for idx, sample in enumerate(samples):
 			self.pca_to_samples[self.texel_embeddings[idx]] = sample
-
-	def ann(self, voxel_embeddings):
-		ann = AnnLite(self.rank, metric='cosine', data_path="/tmp/annlite_data")
+		
+		# Necessary to empty tmp because otherwise previous embeddings will be included 
+		for dir_ in os.listdir('tmp/'):
+			shutil.rmtree(f'tmp/{dir_}')
+		self.ann = AnnLite(self.rank, metric='cosine', data_path="tmp/")
 		docs = DocumentArray.empty(self.texel_embeddings.shape[0])
 		docs.embeddings = self.texel_embeddings
-		ann.index(docs)
+		self.ann.index(docs)
 
+	def approximate_nearest_neighbors(self, voxel_embeddings):
 		query = DocumentArray.empty(voxel_embeddings.shape[0])
 		query.embeddings = voxel_embeddings
-		ann.search(query)
+		self.ann.search(query)
 		examples = torch.vstack(list(self.pca_to_samples.keys()))
 
 		matches = []
 		for q in query:
-			embedding = ann.get_doc_by_id(q.matches[0].id).embedding
-			
+			embedding = q.matches[0].embedding
 			match = self.pca_to_samples[list(self.pca_to_samples.keys())[(embedding == examples).all(axis=1).nonzero()]]
 			matches.append(match)
 		return torch.vstack(matches)
 
 		
-	def pnn(self, voxel_patches):
+	def pynndescent(self, voxel_patches):
 		#Experimenting with pynndescent?
 		indices = pynndescent.NNDescent(voxel_patches)
 		indices.prepare()
@@ -59,9 +62,9 @@ class Search:
 		voxel_patches = einops.rearrange(voxel_patches, 'n p h w c -> (n p) (h w c)').float() # N*3 x 192
 		voxel_embeddings = torch.matmul(voxel_patches, self.V_T)
 		if nn_method == 'ann':
-			matches = self.ann(voxel_embeddings)
+			matches = self.approximate_nearest_neighbors(voxel_embeddings)
 		elif nn_method == 'pnn':
-			matches = self.pnn(voxel_patches)
+			matches = self.pynndescent(voxel_patches)
 		else: 
 			raise Exception("better luck next time")
 		matches = einops.rearrange(matches, '(n p) (h w c) -> n p h w c', n=n, p=p, h=h, w=w, c=c)
@@ -72,11 +75,13 @@ class Search:
 if __name__ == "__main__":
 	texture_file = 'tomatoes.png'
 	texture_dir = 'textures'
-	texture = torchvision.io.read_image(texture_dir + '/' + texture_file).permute(1, 2, 0)[:18]
+	texture = torchvision.io.read_image(texture_dir + '/' + texture_file).permute(1, 2, 0)
 	search = Search(exemplar=texture)
 
 	dummy_voxels = texture[:8,:8, :].unsqueeze(0).unsqueeze(0)
-	plt.imshow(texture[:8,:8, :])
+
+	dummy_voxels = torch.randn([4, 3, 8, 8, 3])
+	plt.imshow(dummy_voxels[0,0,:8,:8, :])
 	plt.show()
 	matches = search.find(dummy_voxels)
 	plt.imshow(matches[0][0].int())
