9d6cbab4cb51fcb3380db7fe12a86b3f1048394f
Rohan Mathur <rohanmathur@berkeley.edu>
2024-04-12 18:03:50 -0700

    started sampling but not finished

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import torch 
 import torchvision
-from torchvision.transforms.functional import pil_to_tensor
 import matplotlib.pyplot as plt
 import tyro
 from pathlib import Path
@@ -11,50 +10,37 @@ import numpy as np
 import time
 import pdb
 
-def sample_voxels(sample_point, radius, mesh, texture, pitch=0.01):
-	# Sample voxel mesh
-	sample_grid = trimesh.voxel.creation.local_voxelize(mesh=mesh, point=sample_point, pitch=pitch, radius=radius).apply_scale(pitch)
-	num_samples = sample_grid.points.shape[0]
-	num_pixels = texture.shape[0] * texture.shape[1]
-	p = torch.ones(num_pixels)/num_pixels
-	index = torch.multinomial(input=p, num_samples=num_samples, replacement=True)
-	sample_colors = texture.view((num_pixels, -1))[index].reshape(sample_grid.points.shape[0], 3).numpy()
-	return sample_grid, sample_colors
 
-def tensorize_mesh(mesh, pitch=0.01):
-	num_points = int((mesh.bounds[1][0] - mesh.bounds[0][0]) / pitch)
-	heights = np.linspace(mesh.bounds[0][0], mesh.bounds[1][0], num_points)
-	max_width = mesh.bounds[1][1] - mesh.bounds[0][1]
-	max_length = mesh.bounds[1][2] - mesh.bounds[0][2]
+def sample_voxel(full_grid: torch.Tensor, batch_size: int = 16) -> torch.Tensor:
+	"""
+	full_grid:  torch.Tensor (z, x, y, 3)
+	batch size: number of voxels sampling for neighborhoods
 
-	sections = mesh.section_multiplane(plane_origin=mesh.centroid, plane_normal=[1,0,0], heights=heights)
-	planes = []
-	max_width = 0
-	max_length = 0
-	for s in sections:
-		if s is not None:
-			new_plane = pil_to_tensor(s.rasterize(pitch=pitch))[0]
-			max_width = max(max_width, new_plane.shape[1])
-			max_length = max(max_length, new_plane.shape[0])
-			planes.append(new_plane)
-	for i in range(len(planes)):
-		width_pad_left = (max_width - planes[i].shape[1]) // 2
-		length_pad_top = (max_length - planes[i].shape[0]) // 2
-		width_pad_right = max_width - planes[i].shape[1] - width_pad_left
-		length_pad_bottom = max_length - planes[i].shape[0] - length_pad_top
-		planes[i] = torch.nn.functional.pad(planes[i], (width_pad_left, width_pad_right, length_pad_top, length_pad_bottom))
-	planes = torch.stack(planes).unsqueeze(3)
-	# make planes rgb
-	planes = planes.repeat(1, 1, 1, 3)
-	return planes
+	return: torch.Tensor (batch_size, 3)
+	"""
+	#select random voxel (16 times)
+	p = torch.ones(full_grid.shape[0] * full_grid.shape[1] * full_grid.shape[2])/(full_grid.shape[0] * full_grid.shape[1] * full_grid.shape[2])
+	index = torch.multinomial(input=p, num_samples=batch_size, replacement=True)
+	index = torch.stack([index // (full_grid.shape[1] * full_grid.shape[2]), \
+					 	(index % (full_grid.shape[1] * full_grid.shape[2])) // full_grid.shape[2], \
+						(index % (full_grid.shape[1] * full_grid.shape[2])) % full_grid.shape[2]], dim=1)
+	return index
 
-def sample_texture(texture, im):
-	num_samples = im.shape[0] * im.shape[1]
-	num_pixels = texture.shape[0] * texture.shape[1]
-	p = torch.ones(num_pixels)/num_pixels
-	index = torch.multinomial(input=p, num_samples=num_samples, replacement=True)
-	init = texture.view((num_samples, -1))[index].reshape(im.shape)
-	return init
+
+
+def sample_neighborhood(full_grid_tensor: torch.Tensor, index: torch.Tensor, neighborhood_dim: int = 8) -> torch.Tensor:
+	"""
+	full_grid_tensor:  torch.Tensor (z, x, y, 3)
+	index:  torch.Tensor (batch_size, 3)
+	neighborhood_dim: (int) dimentions of neighborhood (neighborhood_dim * neighborhood_dim)
+
+	return: torch.Tensor (batch_size, 3, neighborhood_dim, neighborhood_dim, 3)
+	"""
+	#find neighborhood of voxel
+	pdb.set_trace()
+	indices_expanded = index[:, None, :].expand(-1, neighborhood_dim, -1)
+	offsets = torch.arange(-neighborhood_dim // 2, neighborhood_dim // 2).unsqueeze(0).unsqueeze(-1)
+
 
 def main(texture_file: str = 'tomatoes.png', 
 		 object_file: str = "cube.obj", 
@@ -65,7 +51,7 @@ def main(texture_file: str = 'tomatoes.png',
 		 device: str = 'cpu'):
 	
 	# Load and sample texture
-	texture = torchvision.io.read_image(texture_dir + '/' + texture_file).permute(1, 2, 0)
+	texture = torchvision.io.read_image(texture_dir + '/' + texture_file).permute(1, 2, 0).float()/255.0
 	num_samples = texture.shape[0] * texture.shape[1] 
 	p = torch.ones(num_samples)/num_samples # Initialize a uniform distribution over samples
 	index = torch.multinomial(input=p, num_samples=num_samples, replacement=True) #samples from uniform distribution
@@ -81,26 +67,20 @@ def main(texture_file: str = 'tomatoes.png',
 
 	# voxelize mesh
 	start = time.time()
-	full_grid = trimesh.voxel.creation.voxelize(mesh, pitch=pitch).fill() #number of voxels in voxel grid
+	full_grid = trimesh.voxel.creation.voxelize(mesh, pitch=pitch).fill() #number of voxels in voxel grid (depth, length, width)
 	end = time.time()
 	print(f"Voxelized mesh with shape {full_grid.shape} in {end - start:.2f} seconds")
-	num_samples = full_grid.points.shape[0]
+	full_grid_tensor = torch.from_numpy(full_grid.matrix).unsqueeze(-1).expand(-1,-1,-1,3).float()
+	
+	num_samples = full_grid_tensor.shape[0] * full_grid_tensor.shape[1] * full_grid_tensor.shape[2]
 	num_pixels = texture.shape[0] * texture.shape[1]
 	p = torch.ones(num_pixels)/num_pixels
 	index = torch.multinomial(input=p, num_samples=num_samples, replacement=True)
-	full_colors = texture.view((num_pixels, -1))[index].reshape(full_grid.points.shape[0], 3) #gets colors of the sampled pixels and matches the shape of the mesh
-
-	sample_voxels1, sample_colors1 = sample_voxels(np.array([-2.0,-1.0,-1.0]), 50, mesh, texture, pitch=0.01)
-	sample_voxels2, sample_colors2 = sample_voxels(np.array([-2.0,-0.5,-1.0]), 50, mesh, texture, pitch=0.01)
+	sample_colors = texture.view((num_pixels, -1))[index].reshape(full_grid_tensor.shape[0], full_grid_tensor.shape[1], full_grid_tensor.shape[2], 3)
+	full_grid_tensor = full_grid_tensor * sample_colors
 	
+	sample_neighborhood(full_grid_tensor, sample_voxel(full_grid_tensor), neighborhood_dim=8)
 
-	# mesh_tensor = tensorize_mesh(mesh, pitch=0.01)
-	# print(mesh_tensor.shape)
-	# mesh_pointcloud = mesh_tensor.nonzero()[:, :3][::3]
-	# print(mesh_pointcloud.shape)
-	# print(torch.sum(mesh_pointcloud))
-
-  
 	# display mesh
 	if show:
 		# plt.imshow( init )
@@ -119,9 +99,9 @@ def main(texture_file: str = 'tomatoes.png',
 		# display voxels in viser and sample colors from texture
 		server.add_point_cloud(
 			name="/full_grid",
-			points=full_grid.points[::2],
+			points=full_grid_tensor[:, :, :, 0].nonzero().numpy() * pitch,
 			position=(0.0, 0.0, 0.0),
-			colors=full_colors[::2],
+			colors=(255, 0 , 0),
 			wxyz=tf.SO3.from_x_radians(np.pi / 2).wxyz,
 			point_size=0.01,
 		)
