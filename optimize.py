204c6c2712f09f8dfcbb1d10f702a8dc67797354
Catherine Van Keuren <cvankeuren@berkeley.edu>
2024-04-19 13:37:56 -0700

    start of hist

--- optimize.py
+++ optimize.py
@@ -2,8 +2,12 @@ import torch
 import einops 
 
 class Optimize:
-    def __init__(self, r=0.8):
+    def __init__(self, texture, r=0.8, num_bins=16):
         self.r = r
+        self.texture = texture
+        self.texture_hists = torch.stack([self.create_texture_hist(texture, 0), self.create_texture_hist(texture, 1), self.create_texture_hist(texture, 2)], dim=0)
+        self.num_bins = num_bins
+        self.solid_hists = torch.stack
 
     def __call__(self, exemplar: torch.Tensor, solid: torch.Tensor, tol=0.001, max_iter=1000) -> torch.Tensor:
         """
@@ -35,6 +39,26 @@ class Optimize:
 
         #raise to power of r - 2
         return torch.pow(n, (self.r - 2)) # [n, num_axes]
+
+
+    def update_weight(self, weight: torch.Tensor, solid: torch.Tensor, exemplar: torch.Tensor) -> torch.Tensor:
+        """
+        weight:   torch.Tensor (n, 3)
+        solid:    torch.Tensor (n, 3, 8, 8, 3)
+        exemplar: torch.Tensor (n, 3, 8, 8, 3)
+
+        return:   torch.Tensor (n, 3)
+        """
+    # get bin number
+        bin_width = 1/self.num_bins
+        bin_indices = weight//bin_width #(n, 3)
+        n, c = bin_indices.shape
+        hist_values = self.texture_hists[[0, 1, 2]*n, bin_indices.flatten()].shape(bin_indices.shape) #(n,3)
+        
+
+    
+        
+
     
     def closed_form_irls(self, w: torch.Tensor, e: torch.Tensor):
         """
@@ -47,4 +71,21 @@ class Optimize:
             /torch.sum(w)
 
         return s_new
+    
+    def create_texture_hist(self, texture: torch.Tensor, channel: int, num_bins: int) -> torch.Tensor:
+        """
+        texture: torch.Tensor (h, w, 3)
+        channel: int [0, 1, 2] - represents the color chosen
+        num_bins: int - default 16
+
+        returns h: torch.Tensor (# bins)
+        """
+        texels = texture[..., channel].view(-1)
+        h = torch.histogram(texels.float(), bins=num_bins, min=0, max=255)
+
+        return h
+    
+    def create_solid_hist(self, solid: torch.Tensor, channel: int, num_bins: int) -> torch.Tensor:
+        
+
 
